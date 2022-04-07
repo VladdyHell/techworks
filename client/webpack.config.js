857ab124628bec39d710c7cd69e35a3582efc9fe
Vladd Cantor <crimsonvirtuoso@vladdyhell.bbrouter>
2022-04-07 11:49:15 +0800

    remove extra public path for webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,7 +32,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist/"),
     publicPath: "/dist/",
-    publicPath: "/profile/dist/",
     filename: "bundle.js",
   },
   devServer: {
